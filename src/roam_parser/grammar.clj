804e420d4c5b65fe71b9c2817053659d1f83bf54
rainbowCat <jazziblue@gmail.com>
2020-12-29 22:13:50 -0800

    add more tests, remove unused functions

--- src/roam_parser/grammar.clj
+++ src/roam_parser/grammar.clj
@@ -14,7 +14,7 @@
 
 ;;; Grammar Definitions
 (def ANY ".*")
-(def URLS ".*")                                   ;TODO
+(def URLS ".*")                                             ;TODO
 
 (def BOLD (mk-gmr ["\\*\\*" 'ANY "\\*\\*"]))
 (def ITALICS (mk-gmr ["__" 'ANY "__"]))
@@ -28,31 +28,6 @@
 ;;; Grammar Function
 (defn extract-pages
   [x]
-  (re-matches PAGE x))                            ;TODO extract nested pages
-
-;(defmacro gen-basic
-;  [name]
-;  '(defn (str/join ["is-" name])
-;     [x]
-;     (if (not (nil? (re-matches (str/upper-case name) x))) true false)))
-
-(defn is-bold [x] (if (not (nil? (re-matches BOLD x))) true false))
-(defn is-italics [x] (if (not (nil? (re-matches ITALICS x))) true false))
-(defn is-highlight [x] (if (not (nil? (re-matches HIGHLIGHT x))) true false))
-(defn is-page [x] (if (not (nil? (re-matches PAGE x))) true false))
-(defn is-ref [x] (if (not (nil? (re-matches REF x))) true false))
-(defn is-alias [x] (if (not (nil? (re-matches ALIAS x))) true false))
-(defn is-img [x] (if (not (nil? (re-matches IMGS x))) true false))
-
-;  '(defn (str/join ["is-" name])
-;     [x]
-;     (if (not (nil? (re-matches (str/upper-case name) x))) true false)))
-
-;(defn is-page
-;  [x]
-;  (def p (extract-pages x))
-;  (if (not (nil? p))
-;    (do (println "Found PAGE:") true)
-;    false))
+  (re-matches PAGE x))                                      ;TODO extract nested pages
 
 
--- test/roam_parser/basic_test.clj
+++ test/roam_parser/basic_test.clj
@@ -1,19 +1,39 @@
 (ns roam-parser.basic-test
-  (:require [clojure.test :refer :all]
-            [roam-parser.core :refer :all]
-            [roam-parser.grammar :refer :all]))
+  (:require [clojure.test :refer [deftest is are]]
+    ;[roam-parser.core :refer :all]
+            [roam-parser.grammar :refer :all]
+    ;[expectations.clojure.test :refer [expect]]
+            ))
 
-(def a-page "[[A]]")
-(def not-page "[A]")
+(defn check [pattern input] (not (nil? (re-matches pattern input))))
+
+(deftest simple-bold
+  (is (check BOLD "**Abc**"))
+  (is (not (check BOLD "*A"))))
+
+(deftest simple-italics
+  (is (check ITALICS "__Abc__"))
+  (is (not (check ITALICS "_A"))))
+
+(deftest simple-highlight
+  (is (check HIGHLIGHT "^^Abc^^"))
+  (is (not (check HIGHLIGHT "^A"))))
 
 (deftest simple-page
-  (is (is-page a-page))
-  (is (not (is-page not-page))))
-
-
-(def a-ref "((A))")
-(def not-ref "(A)")
+  (is (check PAGE "[[A]]"))
+  (is (not (check PAGE "[A]"))))
 
 (deftest simple-ref
-  (is (is-page a-ref))
-  (is (not (is-page not-ref))))
+  (is (check REF "((A))"))
+  (is (not (check REF "(A)"))))
+
+(deftest simple-alias
+  (is (check ALIAS "[alias text](www.google.com)"))
+  (is (check ALIAS "[alias text]([[abcdef]])"))             ;alias to a page
+  (is (check ALIAS "[alias text](((abcdef)))"))             ;alias to a block reference
+  (is (not (check ALIAS "[alias text](((abcdef))")))        ; missing parentheses
+  (is (not (check ALIAS "[alias text(((abcdef))"))))        ; missing brackets
+
+(deftest simple-img
+  (is (check IMGS "![](https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o/imgs%2Fapp%2Faprilsin%2FGtBI5zPC0q.png?alt=media&token=ac735fc8-ad62-4c9c-92cb-dc8084ab9031)"))
+  (is (not (check IMGS "![]https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o/imgs%2Fapp%2Faprilsin%2FGtBI5zPC0q.png?alt=media&token=ac735fc8-ad62-4c9c-92cb-dc8084ab9031)"))))
